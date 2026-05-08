b72adbb81d62918c36c61857acf9445d32650666
Spencer Muller <spencer.muller@edu.sait.ca>
2026-05-07 23:42:16 -0600

    scaffold monogram-lsp project with OmniSharp LSP and solution wiring

---
+++ monogram-lsp/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using monogram_lsp;
+using monogram_lsp.Handlers;
+using OmniSharp.Extensions.LanguageServer.Server;
+
+var server = await LanguageServer.From(options => options
+    .WithInput(Console.OpenStandardInput())
+    .WithOutput(Console.OpenStandardOutput())
+    .ConfigureLogging(x => x
+        .AddLanguageProtocolLogging()
+        .SetMinimumLevel(LogLevel.Warning))
+    .WithServices(s => s.AddSingleton<DocumentStore>())
+    .WithHandler<TextDocumentHandler>()
+);
+
+await server.WaitForExit;
